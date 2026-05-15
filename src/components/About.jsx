8d4bc6c9bf9ccc532e3c129d1aa3fa298ec68e03
Angelique Liard <liardangelqiue@gmail.com>
2026-05-15 09:54:08 +0200

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,33 @@
+function About() {
+  return (
+    <>
+      <section className="about">
+        <h2>À propos</h2>
+        <p>
+          Après plusieurs années dans la vente et l'accompagnement d'enfants,
+          j'ai choisi de me reconvertir dans le développement web.
+        </p>
+        <p>
+          J'ai découvert le développement web en 2025 et je me suis lancée dans
+          une formation de développeur web et web mobile full stack à distance
+          avec le Centre Européen de Formation.
+        </p>
+
+        <p>
+          Après un an de formation, j'ai acquis les compétences nécessaires pour
+          évoluer en tant que développeuse web junior. Il me reste désormais un
+          stage en entreprise à effectuer afin de valider ma formation et
+          obtenir mon titre professionnel.
+        </p>
+
+        <p>
+          Je développe actuellement mes compétences en HTML, CSS, JavaScript,
+          React et PHP à travers différents projets personnels et de formation.
+        </p>
+      </section>
+      <div className="line"></div>
+    </>
+  );
+}
+
+export default About;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <h1>Mon portfolio</h1>
+      <h2>Liard Angélique</h2>
+      <h2>Développeuse web junior</h2>
+      <button>Voir mes projets</button>
+      <div className="line"></div>
+    </header>
+  );
+}
+
+export default Header;
